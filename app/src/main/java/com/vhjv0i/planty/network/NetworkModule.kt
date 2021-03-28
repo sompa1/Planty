2c142a87efeaf56006aa3371c451b1ee4976c1bd
Panna Somlo <panna.somlo@sap.com>
2021-03-28 18:15:37 +0200

    Add network components

---
+++ app/src/main/java/com/vhjv0i/planty/network/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.vhjv0i.planty.network
+
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit.Builder? {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+    }
+
+    @Provides
+    @Singleton
+    fun providePlantsApi(retrofitBuilder: Retrofit.Builder): PlantsApi? {
+        return retrofitBuilder.baseUrl(NetworkConfig.API_ENDPOINT_ADDRESS).build()
+            .create(PlantsApi::class.java)
+    }
+}
---
+++ app/src/main/java/com/vhjv0i/planty/network/PlantsApi.kt
@@ -0,0 +1,4 @@
+package com.vhjv0i.planty.network
+
+interface PlantsApi {
+}
