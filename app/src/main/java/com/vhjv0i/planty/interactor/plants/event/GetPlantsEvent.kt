74fb63fe703f5163824385b7b7ac7d9e3db168c3
Panna Somlo <panna.somlo@sap.com>
2021-04-11 22:25:29 +0200

    Finish networking

--- app/src/main/java/com/vhjv0i/planty/interactor/plants/event/GetPlantsEvent.kt
+++ app/src/main/java/com/vhjv0i/planty/interactor/plants/event/GetPlantsEvent.kt
@@ -1,4 +1,7 @@
 package com.vhjv0i.planty.interactor.plants.event
 
-class GetPlantsEvent {
-}
+import io.swagger.client.models.SpeciesLight
+
+data class GetPlantsEvent(var code: Int = 0,
+                          var plants: List<SpeciesLight>? = null,
+                          var throwable: Throwable? = null)
