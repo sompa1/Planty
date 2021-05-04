4bae5cbf90c9701b53a99ebbf009d627c8ea2e35
Panna Somlo <panna.somlo@sap.com>
2021-05-04 20:49:39 +0200

    Integrate Analytics and Crashlytics

--- app/src/main/java/com/vhjv0i/planty/ui/about/AboutFragment.kt
+++ app/src/main/java/com/vhjv0i/planty/ui/about/AboutFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -27,6 +28,10 @@ class AboutFragment : Fragment() {
                 ViewModelProvider(this).get(AboutViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_about, container, false)
         val textView: TextView = root.findViewById(R.id.text_about)
+        val crashButton: Button = root.findViewById(R.id.crashButton)
+        crashButton.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
+        }
         aboutViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
